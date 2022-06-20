2e8c9a893c4bf5fd090fcd27e8efa1b3c23fe35f
haqsir@163.com <haqsir@163.com>
2022-06-21 00:57:50 +0800

    小蜜蜂

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,6 @@
-<script setup lang="ts">
-import Honeybee from './components/honeybee.vue'
-</script>
-
 <template>
   <Honeybee />
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<script setup lang="ts">
+import Honeybee from './components/honeybee.vue'
+</script>
--- src/components/honeybee.vue
+++ src/components/honeybee.vue
@@ -1,9 +1,21 @@
 <template>
-<div class="honeybee"></div>
+<div class="honeybee">
+    <div class="honeybee-body"></div>
+</div>
 </template>
 <script setup lang="ts">
 
 </script>
 <style lang="scss" scoped>
+.honeybee{
 
+}
+.honeybee-body{
+    width: 150px;
+    height: 90px;
+    background-color: #ffd400;
+    background-image: radial-gradient(circle at 75% 45%, #ffd400 42%, #8a4514 51%, #8a4514 54%, #ffd400 64%, #ffd400 66%, #8a4514 74%, #8a4514 77%, #ffd400 86%);
+    border-radius: calc(12 * 1vh) calc(9 * 1vh) calc(9 * 1vh) calc(6 * 1vh);
+    z-index: 1;
+}
 </style>
